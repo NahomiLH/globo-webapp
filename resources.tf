4b23fd83d2e3bbf6497a0959693f8b896b0ba2b6
Lopez <n.lopez.hernandez@accenture.com>
2024-02-18 00:04:49 -0600

    Fixing again

--- resources.tf
+++ resources.tf
@@ -32,10 +32,10 @@ resource "aws_iam_instance_profile" "main" {
 }
 
 resource "aws_instance" "main" {
-  count                = length(data.tfe_outputs.networking.nonsensitive_values.public_subnets)
-  ami                  = nonsensitive(data.aws_ssm_parameter.amzn2_linux.value)
-  instance_type        = var.instance_type
-  subnet_id            = data.tfe_outputs.networking.nonsensitive_values.public_subnets[count.index]
+  count         = length(data.tfe_outputs.networking.nonsensitive_values.public_subnets)
+  ami           = nonsensitive(data.aws_ssm_parameter.amzn2_linux.value)
+  instance_type = var.instance_type
+  subnet_id     = data.tfe_outputs.networking.nonsensitive_values.public_subnets[count.index]
   vpc_security_group_ids = [
     aws_security_group.webapp_http_inbound_sg.id,
     aws_security_group.webapp_ssh_inbound_sg.id,
@@ -48,14 +48,16 @@ resource "aws_instance" "main" {
     "Name" = "${local.name_prefix}-webapp-${count.index}"
   })
 
-  user_data_replace_on_change = true
-
-  user_data = templatefile("./templates/userdata.sh", {
+  user_data = templatefile("${path.module}/templates/userdata.sh", {
     playbook_repository = var.playbook_repository
     secret_id           = var.api_key_secret_id
     host_list_ssm_name  = local.host_list_ssm_name
     site_name_ssm_name  = local.site_name_ssm_name
   })
+
+  user_data_replace_on_change = true
+  iam_instance_profile        = aws_iam_instance_profile.main.name
+
 }
 
 resource "aws_lb" "main" {
@@ -93,4 +95,4 @@ resource "aws_alb_target_group_attachment" "main" {
   count            = length(aws_instance.main.*.id)
   target_group_arn = aws_lb_target_group.main.arn
   target_id        = aws_instance.main[count.index].id
-}5K
+}
